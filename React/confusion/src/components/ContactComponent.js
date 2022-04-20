99a31aca5234660c387cb61dea9a227e855742db
cjochoa0 <cjochoa@ualr.edu>
2022-04-20 16:32:20 -0500

    Guide 34 Uncontrolled forms done

--- React/confusion/src/components/ContactComponent.js
+++ React/confusion/src/components/ContactComponent.js
@@ -80,6 +80,17 @@ class Contact extends Component {
 
         return(
             <div className="row row-content">
+                <div className="col-12 col-sm-11 offset-sm-1">
+                    <div className="btn-group" role="group">
+                        <a role="button" className="btn btn-primary"
+                           href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                        <a role="button" className="btn btn-info"><i
+                            className="fa fa-skype"></i> Skype</a>
+                        <a role="button" className="btn btn-success"
+                           href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i>
+                            Email</a>
+                    </div>
+                </div>
                 <div className="col-12">
                     <h3>Send us your Feedback</h3>
                 </div>
